d0a7953bcb8187a637e7329b926b68a536bd7980
Anton <anton.vidinev@da-14.com>
2022-02-16 03:25:44 +0200

    add gtm

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -10,6 +10,11 @@ export class NewsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    (window as any).dataLayer.push({
+      'event' : 'my_page_view',
+      'loginStatus' : 'logged in',
+      'pageType' : 'news'
+    });
   }
 
 }
